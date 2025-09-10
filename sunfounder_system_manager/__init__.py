0707c4be48774374f9f7c8c58aa36056f9190f7b
Cavon <cavonxx@gmail.com>
2025-09-10 17:22:23 +0800

    refactor: update import path for create_luancher
    Use absolute import path from sunfounder_service_node instead of relative import to improve code clarity and maintainability

--- sunfounder_system_manager/__init__.py
+++ sunfounder_system_manager/__init__.py
@@ -1,4 +1,4 @@
-from .service_node import create_luancher
+from sunfounder_service_node.service_node import create_luancher
 from .system_manager import SystemManager
 
 main = create_luancher(SystemManager, "system_manager", "SunFounder System manager")
